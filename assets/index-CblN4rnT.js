4322db6c4c1523d66f06e16a80be3067153d8918
Terry <kdjtiger@gmail.com>
2025-03-11 23:04:51 +0900

    Updates

--- assets/index-CblN4rnT.js
+++ assets/index-CblN4rnT.js
@@ -101,11 +101,11 @@ const Headers = (movie) => {
       <div class="overlay" aria-hidden="true" style="background-image: url('${backdrop}');"></div>
       <div class="top-rated-container">
         <h1 class="logo">
-          <img src="./images/logo.png" alt="MovieList" />
+          <img src="../../images/logo.png" alt="MovieList" />
         </h1>
         <div class="top-rated-movie">
           <div class="rate">
-            <img src="./images/star_empty.png" class="star" />
+            <img src="../../images/star_empty.png" class="star" />
             <span class="rate-value">${rate}</span>
           </div>
           <div class="title">${title}</div>
@@ -142,7 +142,7 @@ const MovieItem = (props) => {
             />
             <div class="item-desc">
                 <p class="rate">
-                    <img src="./images/star_empty.png" class="star" />
+                    <img src="../../images/star_empty.png" class="star" />
                     <span>${rate}</span>
                 </p>
                 <strong>${title}</strong>
